ff1b49bc38ea91cef3ae7caad2d7b6ad703ff5b7
rgalinas26 <52674802+rgalinas26@users.noreply.github.com>
2019-12-03 10:47:41 -0500

    FEW 300 start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { GiftGivingModule } from './features/gift-giving/gift-giving.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
+  // You have to make sure that you import these correctly. Put the AppRoutingModule last. It always goes last. This is because when the
+  // app-routing-module might see the AppRoutingModule route before it sees what is below it (child routes)
+  // and redirect your use to home due to the default route.
   imports: [
     BrowserModule,
+    GiftGivingModule,
     AppRoutingModule
   ],
   providers: [],
